37bca37abe0d74302559ec9c524cc360fcea1d7c
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2025-10-28 20:48:56 +0400

    Created First Spring Rest application

---
+++ Library/src/test/java/com/library/dea/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.dea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
