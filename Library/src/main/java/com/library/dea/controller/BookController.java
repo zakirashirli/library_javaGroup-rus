d28c780fa23fddf40dbea77967c7598fde8ba828
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2025-10-28 21:54:48 +0400

    Несколько книг

--- Library/src/main/java/com/library/dea/controller/BookController.java
+++ Library/src/main/java/com/library/dea/controller/BookController.java
@@ -4,11 +4,18 @@ import com.library.dea.model.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class BookController {
 
     @GetMapping("/books")
-    public Book getBook() {
-        return new Book("Spring for begginers", "Mirali");
+    public List<Book> getAllBook() {
+        return List.of(
+                new Book("Spring for begginers", "Mirali", 99.99),
+                new Book("Sherlock Homes", "Aylin", 200.0),
+                new Book("Rich Dad and Poor Dad", "Mirali", 99.99),
+                new Book("Sidney Sheldon", "Shaxriyar", 400.0)
+        );
     }
 }
--- Library/src/main/java/com/library/dea/model/Book.java
+++ Library/src/main/java/com/library/dea/model/Book.java
@@ -3,11 +3,13 @@ package com.library.dea.model;
 public class Book {
     private String title;
     private String author;
+    private Double price;
 
     // constructor
-    public Book(String title, String author){
+    public Book(String title, String author, Double price){
         this.title = title;
         this.author = author;
+        this.price = price;
     }
 
     //getters
@@ -17,5 +19,9 @@ public class Book {
 
     public String getAuthor() {
         return author;
+    }
+
+    public Double getPrice() {
+        return price;
     }
 }
