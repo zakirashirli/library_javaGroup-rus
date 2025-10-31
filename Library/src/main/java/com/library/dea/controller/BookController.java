0645c03d0ecea2dd9856d5ffedfd801fc4841257
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2025-10-31 22:53:39 +0400

    PathVarible and RequestParam

--- Library/src/main/java/com/library/dea/controller/BookController.java
+++ Library/src/main/java/com/library/dea/controller/BookController.java
@@ -1,21 +1,33 @@
 package com.library.dea.controller;
 
 import com.library.dea.model.Book;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/books")
 public class BookController {
 
-    @GetMapping("/books")
-    public List<Book> getAllBook() {
-        return List.of(
-                new Book("Spring for begginers", "Mirali", 99.99),
-                new Book("Sherlock Homes", "Aylin", 200.0),
-                new Book("Rich Dad and Poor Dad", "Mirali", 99.99),
-                new Book("Sidney Sheldon", "Shaxriyar", 400.0)
-        );
+    private final List<Book> books = List.of(
+            new Book(1,"Spring for begginers", "Mirali", 99.99),
+            new Book(2,"Sherlock Homes", "Aylin", 200.0),
+            new Book(3,"Rich Dad and Poor Dad", "Mirali", 99.99),
+            new Book(4,"Sidney Sheldon", "Shaxriyar", 400.0));
+
+    // 1 показать все книнги
+    @GetMapping("/all")
+    public List<Book> getAllBooks() {
+        return books;
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Integer id) {
+        for (Book book : books) {
+            if (book.getId().equals(id)){
+                return book;
+            }
+        }
+        return null;
     }
 }
--- Library/src/main/java/com/library/dea/model/Book.java
+++ Library/src/main/java/com/library/dea/model/Book.java
@@ -1,18 +1,22 @@
 package com.library.dea.model;
 
 public class Book {
+    private Integer id;
     private String title;
     private String author;
     private Double price;
 
     // constructor
-    public Book(String title, String author, Double price){
+    public Book(Integer id,String title, String author, Double price){
+        this.id = id;
         this.title = title;
         this.author = author;
         this.price = price;
     }
 
     //getters
+    public Integer getId(){return id;}
+
     public String getTitle() {
         return title;
     }
